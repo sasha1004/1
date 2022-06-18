a81e969e02c6cd1103349c84d0465e426f4f28f8
panda <mail@mail.ru>
2022-06-18 14:03:38 +0300

    asd

---
+++ src/ru/skypro/test.java
@@ -0,0 +1,14 @@
+package ru.skypro;
+
+public class test {
+
+    asfasfasf
+    s
+            af
+    as
+                    floatas
+    floatasasf
+                            a
+
+    sfasf
+}
